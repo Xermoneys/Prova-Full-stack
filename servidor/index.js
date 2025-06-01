2f9cd276a9e1328597f46e5a21675aaf2eb164f9
Computador casa <lucianomonegatto@gmail.com>
2025-06-01 17:36:42 -0300

    site

--- servidor/index.js
+++ servidor/index.js
@@ -2,6 +2,14 @@ var http = require("http");
 
 var express = require("express");
 
+var mongodb = require("mongodb")
+
+const uri = "mongodb+srv://luciano:240904lu@projeto.vgvfhxf.mongodb.net/?retryWrites=true&w=majority&appName=Projeto";
+
+const MongoClient = mongodb.MongoClient;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 var bodyParser = require("body-parser");
 
 var app = express() ;
@@ -16,10 +24,35 @@ var server = http. createServer (app);
 server.listen(80);
 console.log('Servidor rodando ...');
 
-app.post('/cadastro' ,function (requisicao , resposta){
-    var nome = requisicao. body.nome;
-    var nota1 = requisicao. body.nota1;
-    var nota2 = requisicao. body.nota2;
-    var nota3 = requisicao. body.nota3;
-resposta.render('home_lista' , {nome, nota1 , nota2 , nota3})
-})
+client.connect().then(() => {
+    const dbo = client.db("exemplo_bd"); //conecta no banco de dados
+    const usuarios = dbo.collection("usuarios");
+
+app.post('/cadastro', function (requisicao, resposta) {
+    const data = {
+        nome: requisicao.body.nome,
+        nota1: requisicao.body.nota1,
+        nota2: requisicao.body.nota2,
+        nota3: requisicao.body.nota3
+    };
+
+    usuarios.insertOne(data, (err) => {
+        if (err) {
+            resposta.status(400).json({ success: false, message: "Erro ao cadastrar usuário!" });
+        } else {
+            resposta.redirect('/lista');
+        }
+    });
+});
+
+app.get('/lista', function (requisicao, resposta) {
+    usuarios.find().toArray((err, resultado) => {
+        if (err) {
+            resposta.status(500).send("Erro ao buscar usuários.");
+        } else {
+            resposta.render('home_lista', { usuarios: resultado });
+        }
+    });
+});
+
+});
